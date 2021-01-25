ca1ab9b48f8c0f718436d4e6d09a770a0a8a756a
dankondr <dankondr@icloud.com>
2021-01-25 17:09:29 +0300

    Start leftPadString function development

--- src/Functions/registerFunctionsString.cpp
+++ src/Functions/registerFunctionsString.cpp
@@ -35,6 +35,7 @@ void registerFunctionNormalizedQueryHash(FunctionFactory &);
 void registerFunctionCountMatches(FunctionFactory &);
 void registerFunctionEncodeXMLComponent(FunctionFactory & factory);
 void registerFunctionDecodeXMLComponent(FunctionFactory & factory);
+void registerFunctionLeftPadString(FunctionFactory & factory);
 
 
 #if USE_BASE64
@@ -73,6 +74,7 @@ void registerFunctionsString(FunctionFactory & factory)
     registerFunctionCountMatches(factory);
     registerFunctionEncodeXMLComponent(factory);
     registerFunctionDecodeXMLComponent(factory);
+    registerFunctionLeftPadString(factory);
 #if USE_BASE64
     registerFunctionBase64Encode(factory);
     registerFunctionBase64Decode(factory);
